470b8fcb06f0ebc2be52619038ed020dc483b3cc
mw <willia32@hotmail.com>
2017-06-28 23:52:00 -0700

    Establish routing from root to the final welcome page

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def signup():
-    return render_template('index.html', title="signup")
+def index():
+    return render_template('index.html', title="hello")
 
-@app.route('/welcome')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html', title="signup")
+
+@app.route('/welcome', methods=['POST'])
 def welcome():
-    return render_template('signup_confirmation.html', title="welcome", username="Michael")
+    username = request.form['username']
+    pw_1 = request.form['pw_1']
+    pw_2 = request.form['pw_2']
+    email = request.form['email']
+
+    return render_template('signup_confirmation.html', title='welcome', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
